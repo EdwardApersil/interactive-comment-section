563aa23cb542f30a34053be65f6b96b673e6a512
Edward <edwardapersil7@gmail.com>
2024-01-19 03:52:35 +0000

    restructure the code base

--- index.js
+++ index.js
@@ -1,206 +1,234 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const plus = document.querySelector(".plus");
-  const minus = document.querySelector(".minus");
-  const counter = document.querySelector(".counter");
-  const btn = document.querySelector(".button");
-  const commentsContainer = document.getElementById("commentsContainer");
-  const replyContainer = document.getElementById("replyContainer");
-  const reply = document.querySelector(".addReply");
-  const repBtn = document.querySelector(".rep-btn");
-  const rep = document.querySelector(".buttReply");
+const data = {
+  currentUser: {
+    image: {
+      png: "./images/avatars/image-juliusomo.png",
+      webp: "./images/avatars/image-juliusomo.webp",
+    },
+    username: "juliusomo",
+  },
+  comments: [
+    {
+      parent: 0,
+      id: 1,
+      content:
+        "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+      createdAt: "1 month ago",
+      score: 12,
+      user: {
+        image: {
+          png: "./images/avatars/image-amyrobson.png",
+          webp: "./images/avatars/image-amyrobson.webp",
+        },
+        username: "amyrobson",
+      },
+      replies: [],
+    },
+    {
+      parent: 0,
+      id: 2,
+      content:
+        "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+      createdAt: "2 weeks ago",
+      score: 5,
+      user: {
+        image: {
+          png: "./images/avatars/image-maxblagun.png",
+          webp: "./images/avatars/image-maxblagun.webp",
+        },
+        username: "maxblagun",
+      },
+      replies: [
+        {
+          parent: 2,
+          id: 1,
+          content:
+            "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+          createdAt: "1 week ago",
+          score: 4,
+          replyingTo: "maxblagun",
+          user: {
+            image: {
+              png: "./images/avatars/image-ramsesmiron.png",
+              webp: "./images/avatars/image-ramsesmiron.webp",
+            },
+            username: "ramsesmiron",
+          },
+        },
+        {
+          parent: 2,
+          id: 1,
+          content:
+            "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+          createdAt: "2 days ago",
+          score: 2,
+          replyingTo: "ramsesmiron",
+          user: {
+            image: {
+              png: "./images/avatars/image-juliusomo.png",
+              webp: "./images/avatars/image-juliusomo.webp",
+            },
+            username: "juliusomo",
+          },
+        },
+      ],
+    },
+  ],
+};
+function appendFrag(frag, parent) {
+  var children = [].slice.call(frag.childNodes, 0);
+  parent.appendChild(frag);
+  //console.log(children);
+  return children[1];
+}
 
+const addComment = (body, parentId, replyTo = undefined) => {
+  let commentParent =
+    parentId === 0
+      ? data.comments
+      : data.comments.filter((c) => c.id == parentId)[0].replies;
+  let newComment = {
+    parent: parentId,
+    id:
+      commentParent.length == 0
+        ? 1
+        : commentParent[commentParent.length - 1].id + 1,
+    content: body,
+    createdAt: "Now",
+    replyingTo: replyTo,
+    score: 0,
+    replies: parent == 0 ? [] : undefined,
+    user: data.currentUser,
+  };
+  commentParent.push(newComment);
+  initComments();
+};
+const deleteComment = (commentObject) => {
+  if (commentObject.parent == 0) {
+    data.comments = data.comments.filter((e) => e != commentObject);
+  } else {
+    data.comments.filter((e) => e.id === commentObject.parent)[0].replies =
+      data.comments
+        .filter((e) => e.id === commentObject.parent)[0]
+        .replies.filter((e) => e != commentObject);
+  }
+  initComments();
+};
 
-  let counterValue = 0;
-  counter.textContent = counterValue;
+const promptDel = (commentObject) => {
+  const modalWrp = document.querySelector(".modal-wrp");
+  modalWrp.classList.remove("invisible");
+  modalWrp.querySelector(".yes").addEventListener("click", () => {
+    deleteComment(commentObject);
+    modalWrp.classList.add("invisible");
+  });
+  modalWrp.querySelector(".no").addEventListener("click", () => {
+    modalWrp.classList.add("invisible");
+  });
+};
 
-  plus.addEventListener("click", () => {
-    counter.textContent = ++counterValue;
+const spawnReplyInput = (parent, parentId, replyTo = undefined) => {
+  if (parent.querySelectorAll(".reply-input")) {
+    parent.querySelectorAll(".reply-input").forEach((e) => {
+      e.remove();
+    });
+  }
+  const inputTemplate = document.querySelector(".reply-input-template");
+  const inputNode = inputTemplate.content.cloneNode(true);
+  const addedInput = appendFrag(inputNode, parent);
+  addedInput.querySelector(".bu-primary").addEventListener("click", () => {
+    let commentBody = addedInput.querySelector(".cmnt-input").value;
+    if (commentBody.length == 0) return;
+    addComment(commentBody, parentId, replyTo);
+  });
+};
+
+const createCommentNode = (commentObject) => {
+  const commentTemplate = document.querySelector(".comment-template");
+  var commentNode = commentTemplate.content.cloneNode(true);
+  commentNode.querySelector(".usr-name").textContent =
+    commentObject.user.username;
+  commentNode.querySelector(".usr-img").src = commentObject.user.image.webp;
+  commentNode.querySelector(".score-number").textContent = commentObject.score;
+  commentNode.querySelector(".cmnt-at").textContent = commentObject.createdAt;
+  commentNode.querySelector(".c-body").textContent = commentObject.content;
+  if (commentObject.replyingTo)
+    commentNode.querySelector(".reply-to").textContent =
+      "@" + commentObject.replyingTo;
+
+  commentNode.querySelector(".score-plus").addEventListener("click", () => {
+    commentObject.score++;
+    initComments();
   });
 
-  minus.addEventListener("click", () => {
-    counter.textContent = --counterValue < 0 ? 0 : counterValue;
+  commentNode.querySelector(".score-minus").addEventListener("click", () => {
+    commentObject.score--;
+    if (commentObject.score < 0) commentObject.score = 0;
+    initComments();
   });
+  if (commentObject.user.username == data.currentUser.username) {
+    commentNode.querySelector(".comment").classList.add("this-user");
+    commentNode.querySelector(".delete").addEventListener("click", () => {
+      promptDel(commentObject);
+    });
+    commentNode.querySelector(".edit").addEventListener("click", (e) => {
+      const path = e.path[3].querySelector(".c-body");
+      if (
+        path.getAttribute("contenteditable") == false ||
+        path.getAttribute("contenteditable") == null
+      ) {
+        path.setAttribute("contenteditable", true);
+        path.focus()
+      } else {
+        path.removeAttribute("contenteditable");
+      }
+      
+    });
+    return commentNode;
+  }
+  return commentNode;
+};
 
-  const names = [
-    "amyrobson",
-    "maxblagun",
-    "ramsesmiron",
-    "juliusomo",
-    "jessicachan",
-    "jonathanglenn",
-    "kaitlynguan",
-    "kieronhyde",
-    "luisvelazquez",
-    "michellefryer",
-    "nathanfoster",
-    "sarahconnor",
-    "timjohnson",
-    "tomcruise",
-    "tomhanks",
-    "tomholland",
-    "tommyshelby",
-    "williamturner",
-    "zacharyefron",
-    "zaynmalik",
-  ];
-
-  btn.addEventListener("click", () => {
-    const inputElement = document.querySelector(".input");
-    const input = inputElement.value;
-    const randomName = names[Math.floor(Math.random() * names.length)];
-    const user = {
-      username: randomName,
-      image: { png: "/images/avatars/image-amyrobson.webp" },
-    };
-
-    const currentDate = new Date();
-
-    const formattedDate = formatDate(currentDate);
-    const formattedTime = formatTime(currentDate);
-
-    const newComment = {
-      user,
-      content: input,
-      createdAt: formattedDate,
-      createTm: formattedTime,
-      counter: counterValue,
-    };
-
-    const commentHTML = createCommentHTML(newComment);
-    commentsContainer.innerHTML += commentHTML;
-    inputElement.value = "";
+const appendComment = (parentNode, commentNode, parentId) => {
+  const bu_reply = commentNode.querySelector(".reply");
+  // parentNode.appendChild(commentNode);
+  const appendedCmnt = appendFrag(commentNode, parentNode);
+  const replyTo = appendedCmnt.querySelector(".usr-name").textContent;
+  bu_reply.addEventListener("click", () => {
+    if (parentNode.classList.contains("replies")) {
+      spawnReplyInput(parentNode, parentId, replyTo);
+    } else {
+      //console.log(appendedCmnt.querySelector(".replies"));
+      spawnReplyInput(
+        appendedCmnt.querySelector(".replies"),
+        parentId,
+        replyTo
+      );
+    }
   });
+};
 
-  rep.addEventListener("click", () => {
-    // alert("Click")
-    const inputElement = document.querySelector(".inputRep");
-    const inputRep = inputElement.value;
-    const randomName = names[Math.floor(Math.random() * names.length)];
-    const user = {
-      username: randomName,
-      image: { png: "/images/avatars/image-amyrobson.webp" },
-    };
-
-    const currentDate = new Date();
-
-    const formattedDate = formatDate(currentDate);
-    const formattedTime = formatTime(currentDate);
-
-    const newReply = {
-      user,
-      content: inputRep,
-      createdAt: formattedDate,
-      createTm: formattedTime,
-    };
-
-    const replyHTML = createReplyHTML(newReply);
-    replyContainer.innerHTML += replyHTML;
-    inputElement.value = "";
+function initComments(
+  commentList = data.comments,
+  parent = document.querySelector(".comments-wrp")
+) {
+  parent.innerHTML = "";
+  commentList.forEach((element) => {
+    var parentId = element.parent == 0 ? element.id : element.parent;
+    const comment_node = createCommentNode(element);
+    if (element.replies && element.replies.length > 0) {
+      initComments(element.replies, comment_node.querySelector(".replies"));
+    }
+    appendComment(parent, comment_node, parentId);
   });
+}
 
-  repBtn.addEventListener("click", () => {
-    // alert("Please enter")
-    reply.classList.toggle("addShow");
-  });
+initComments();
+const cmntInput = document.querySelector(".reply-input");
+cmntInput.querySelector(".bu-primary").addEventListener("click", () => {
+  let commentBody = cmntInput.querySelector(".cmnt-input").value;
+  if (commentBody.length == 0) return;
+  addComment(commentBody, 0);
+  cmntInput.querySelector(".cmnt-input").value = "";
 });
 
-function formatDate(date) {
-  const year = date.getFullYear();
-  const month = padZero(date.getMonth() + 1); // Months are zero-based, so add 1
-  const day = padZero(date.getDate());
-  return `${year}-${month}-${day}`;
-}
-
-function formatTime(date) {
-  const hours = padZero(date.getHours());
-  const minutes = padZero(date.getMinutes());
-  const seconds = padZero(date.getSeconds());
-  return `${hours}:${minutes}:${seconds}`;
-}
-
-function padZero(num) {
-  return num < 10 ? "0" + num : num;
-}
-
-const count = 0;
-
-function createCommentHTML(comment) {
-  return `
-
-    <div class="comments">
-        <div class="count">
-            <div class="plus" onclick="incrementCounter(this)"><img src="/images/icon-plus.svg" alt=""></div>
-            <div class="counter">${comment.counter}</div>
-            <div class="minus" onclick="decrementCounter(this)"><img src="/images/icon-minus.svg" alt=""></div>
-        </div>
-        <div class="text">
-            <div class="head">
-                <div class="profile">
-                    <div class="sub">
-                        <div class="image mx-3"><img src="/images/avatars/image-amyrobson.png" height="30" alt=""></div>
-                        <div class="name mx-3">${comment.user.username}</div>
-                    </div>
-                <div class="reply" onclick="toggleReply(this)">
-                    <div class="rep-btn"><img src="/images/icon-reply.svg" alt=""></div>
-                    <div class="rep-text">Reply</div>
-                </div>
-            </div>
-            <p class="content">${comment.content}</p>
-            <div class="date mx-3"> <p>${comment.createdAt}</p>
-                <p class="time">${comment.createTm}</p></div>
-            </div>
-        </div>
-    </div>
-
-    `;
-}
-
-function toggleReply() {
-    const reply = document.querySelector(".addReply");
-    reply.classList.toggle("addShow");
-}
-
-function createReplyHTML(reply) {
-  return `
-    <div class="replySection" data-reply-id="${reply.id}">
-        <div class="text">
-            <div class="head">
-                <div class="profile">
-                    <div class="sub">
-                        <div class="image mx-3"><img src="/images/avatars/image-amyrobson.png" height="30" alt=""></div>
-                        <div class="name mx-3">${reply.user.username}</div>
-                    </div>
-                <div class="reply" onclick="deleteReply(${reply.id})">
-                    <div ><img src="/images/icon-delete.svg" alt=""></div>
-                    <div class="delete-text">Delete</div>
-                </div>
-            </div>
-            <p class="content">${reply.content}</p>
-            <div class="date mx-3"> <p>${reply.createdAt}</p>
-                <p class="time">${reply.createTm}</p></div>
-            </div>
-        </div>
-    </div>
-    `;
-}
-
-function deleteReply(replyId) {
-    const replySection = document.querySelector(`.replySection[data-reply-id="${replyId}"]`);
-    if(replySection){
-        replySection.remove();
-    }
-}
-
-function incrementCounter(element) {
-    const counterElement = element.parentElement.querySelector('.counter');
-    counterElement.textContent = parseInt(counterElement.textContent) + 1;
-  }
-  
-  function decrementCounter(element) {
-    const counterElement = element.parentElement.querySelector('.counter');
-    const currentCounter = parseInt(counterElement.textContent);
-    counterElement.textContent = currentCounter > 0 ? currentCounter - 1 : 0;
-  }
-
-
-  
+// addComment("Hello ! It works !!",0);
